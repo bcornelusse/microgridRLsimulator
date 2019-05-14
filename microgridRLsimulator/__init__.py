ddd2e859b22b49b696ba633c3c89f9e80b1f7ab0
Omar D <omar.drwch@gmail.com>
2019-05-14 08:43:09 +0200

    included setup.py and gym requirement (2)

--- microgridRLsimulator/__init__.py
+++ microgridRLsimulator/__init__.py
@@ -1,3 +1,4 @@
 """
 The microgridRLsimulator package organizes the test-bench functionalities in subpackages.
 """
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+packages_ = find_packages()
+packages = [p for p in packages_ if not(p == 'tests')]
+
+setup(name='microgridRLsimulator',
+      version='0.11dev',
+      description='',
+      url='',
+      author='',
+      author_email='',
+      license='',
+      packages=packages,
+      install_requires=[
+        'python-dateutil', 'docopt==0.6.2', 'matplotlib==3.0.2', 'numpy==1.15.4',
+        'pandas==0.23.4', 'scipy==1.1.0', 'tensorflow==1.12.0', 'tflearn==0.3.2', 'sphinx', 'gym'
+      ],
+      zip_safe=False)
